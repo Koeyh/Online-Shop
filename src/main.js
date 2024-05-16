8568144c5ec2347d1020bc806982110f6fb58eb9
Koeyh <gjcor96@naver.com>
2024-05-16 15:02:41 +0900

    final commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 //Fetch Json file
 function loadItems() {
+    // 외부 리소스를 받아오기 위해 fetch()사용
     return fetch('data/data.json')
         .then((response) => response.json())
         .then((json) => json.items);
 }
 
 function displayItems(items) {
+    // 'items' 클래스의 요소를 찾아 container에 대입
     const container = document.querySelector('.items');
     container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
@@ -22,9 +24,11 @@ function createHTMLString(item) {
 function setEventListeners(items) {
     const logo = document.querySelector('.logo');
     const buttons = document.querySelector('.buttons');
-
+     
+    // logo 클릭 시의 이벤트 리스너 설정
     logo.addEventListener('click', () => displayItems(items));
 
+    // button 클릭 이벤트 리스너 설정
     buttons.addEventListener('click', (event) => {
         onButtonClick(event, items);
     });
